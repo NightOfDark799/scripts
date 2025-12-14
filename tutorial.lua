337c762d47484d5965010e59bec56c5587316256
NightOfDark799 <safakoperasyonuistemem+@gmail.com>
2025-12-14 14:47:00 +0300

    Update tutorial.lua

--- tutorial.lua
+++ tutorial.lua
@@ -1,6 +1,6 @@
 -- roblox make your own r15 scripts tutorial!!!
 -- made by luastun!!
--- video link: https://youtu.be/dvIm08-rE0s
+-- video link: https://youtu.be/RP_Ot5nMHco
 
 local lp = game.Players.LocalPlayer
 local hum = lp.Character.Humanoid
